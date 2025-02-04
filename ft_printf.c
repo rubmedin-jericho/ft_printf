4c646193ca41bfe8321cd26afcc4a25bdd087486
Ruben Medina Martinez <rubmedin@car4s6.42barcelona.com>
2025-02-04 20:35:58 +0100

    push ft_printc.c

--- ft_printf.c
+++ ft_printf.c
@@ -11,63 +11,77 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	prueba_function(char c, char *arg)
-{
-	(void)arg;
-	printf("%c\n", c);
-	if(c == 'c')
-		printf("c\n");
-	else if( c == 's')
-		printf("sS\n");	
-	else if(c == 'p')
-		printf("p\n");
-	else if (c == 'd')
-		printf("d\n");
-	else if (c == 'i')
-		printf("i\n");
-	else if (c == 'u')
-		printf("u\n");
-	else if (c == 'x')
-		printf("x\n");
-	else if (c == 'X')
-		printf("X\n");
-	else if (c == '%')
-		printf("XD\n");
-}
-
 #include <stdio.h> 
 
-int ft_printf(char const *s, ...)
-{	
-	char *str;
-	char *word;
-	int	i;
+void	putnbr_base(int n, int base)
+{
+	
+	char *buff;
 
-	va_list v_args;
-
-	va_start(v_args, s);
-	word = (char *)s;
-	i = 0;
-	while(s[i])
+	buff = "0123456789abcdf";
+	if (n < 0)
 	{
-		//printf("str: %c\n", s[i]);
-		if(s[i] == '%' && ft_isalpha(s[i + 1]))
-		{
-			//printf("ENTRA\n");
-			str = (char *)va_arg(v_args, char const *);
-			prueba_function(word[i + 1], str);	
-		}
-		else
-			//ft_putchar_fd();
-		i++;
+		ft_putchar_fd('-', 1);
+		n = -n;
 	}
-	va_end(v_args);
-	return(0);
+	if (n >= base)
+		putnbr_base((n / base), base);
+	ft_putchar_fd((char)buff[n % base], 1);
 }
+
+//void	switch_function(char c, char *arg)
+//{
+//	(void)arg;
+//	if(c == 'c')
+//		ft_putchar_fd(arg[0], 1);	
+//	else if( c == 's')
+//		ft_putstr_fd(arg, 1);
+//	else if(c == 'p')
+//		putnbr_base(&arg, 16);
+//	else if (c == 'd')
+////		funtion
+//	else if (c == 'i')	
+//		ft_putnbr_fd(ft_atoi(arg), 1);
+//	else if (c == 'u')
+//		printf(" u ");
+//	else if (c == 'x')
+//		printf(" x ");
+//	else if (c == 'X')
+//		printf(" X ");
+//	else if (c == '%')
+//		printf(" XD ");
+//}
+//
+//int ft_printf(char const *s, ...)
+//{	
+//	char *str;
+//	char *word;
+//	int	i;
+//
+//	va_list v_args;
+//
+//	va_start(v_args, s);
+//	word = (char *)s;
+//	i = 0;
+//	while(s[i])
+//	{
+//		if(s[i] != '%')
+//			ft_putchar_fd(s[i], 1);
+//		else if(s[i] == '%' && ft_isalpha(s[i + 1]))
+//		{
+//			str = (char *)va_arg(v_args, char const *);
+//			switch_function(word[i + 1], str);	
+//			i++;
+//		}
+//		i++;
+//	}
+//	va_end(v_args);
+//	return(0);
+//}
 
 int	main()
 {
-	ft_printf("este%s no %csale", "hola", "adios");
+	//ft_printf("este %s no sale", "hola");
+	putnbr_base(15, 16);
 	return (0);
 }
