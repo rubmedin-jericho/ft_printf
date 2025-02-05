381d5ee4a21f28a0e5c09ef370e3502f6a590dbd
Ruben MM <rubenmedinamartinez2016@gmail.com>
2025-02-05 01:04:09 +0100

    push my house

--- ft_printf.c
+++ ft_printf.c
@@ -13,75 +13,107 @@
 #include "ft_printf.h"
 #include <stdio.h> 
 
-void	putnbr_base(int n, int base)
+void	ptrnbr_base(void *nbr, int base)
 {
-	
 	char *buff;
+	unsigned long n;
 
-	buff = "0123456789abcdf";
-	if (n < 0)
-	{
-		ft_putchar_fd('-', 1);
-		n = -n;
-	}
-	if (n >= base)
-		putnbr_base((n / base), base);
-	ft_putchar_fd((char)buff[n % base], 1);
+	n = (unsigned long)nbr;
+	buff = "0123456789abcdef";
+	write(1, "0x", 2);
+	if (n >= (unsigned long)base)
+		ptrnbr_base((n / base), base);
+	ft_putchar_fd(buff[n % base], 1);
 }
 
-//void	switch_function(char c, char *arg)
+//void	putnbr_base(int n, int base)
 //{
-//	(void)arg;
-//	if(c == 'c')
-//		ft_putchar_fd(arg[0], 1);	
-//	else if( c == 's')
-//		ft_putstr_fd(arg, 1);
-//	else if(c == 'p')
-//		putnbr_base(&arg, 16);
-//	else if (c == 'd')
-////		funtion
-//	else if (c == 'i')	
-//		ft_putnbr_fd(ft_atoi(arg), 1);
-//	else if (c == 'u')
-//		printf(" u ");
-//	else if (c == 'x')
-//		printf(" x ");
-//	else if (c == 'X')
-//		printf(" X ");
-//	else if (c == '%')
-//		printf(" XD ");
-//}
+//	char *buff;
 //
-//int ft_printf(char const *s, ...)
-//{	
-//	char *str;
-//	char *word;
+//	buff = "0123456789abcdef";
+//	if((int)n == -2147483648)
+//		write(1, "-2147483648", 10);
+//	if (n < 0)
+//	{
+//		ft_putchar_fd('-', 1);
+//		n = -n;
+//	}
+//	if (n >= (unsigned long)base)
+//		putnbr_base((n / base), base);
+//	ft_putchar_fd(buff[n % base], 1);
+//}
+
+//void	putdec(char *str)
+//{
 //	int	i;
 //
-//	va_list v_args;
-//
-//	va_start(v_args, s);
-//	word = (char *)s;
 //	i = 0;
-//	while(s[i])
+//	while(str[i])
 //	{
-//		if(s[i] != '%')
-//			ft_putchar_fd(s[i], 1);
-//		else if(s[i] == '%' && ft_isalpha(s[i + 1]))
-//		{
-//			str = (char *)va_arg(v_args, char const *);
-//			switch_function(word[i + 1], str);	
-//			i++;
-//		}
+//		if(ft_isdigit(str[i]))
+//			write(1, &str[i], 1);
+//		else if(str[i] == '.' && ft_isdigit(str[i + 1]))
+//			write(1, ".", 1);
 //		i++;
 //	}
-//	va_end(v_args);
-//	return(0);
 //}
+
+void	switch_function(char c, char *arg)
+{
+	(void)arg;
+	if(c == 'c')
+		ft_putchar_fd(arg[0], 1);	
+	else if( c == 's')
+		ft_putstr_fd(arg, 1);
+	else if(c == 'p')
+		ptrnbr_base((unsigned long)arg, 16);
+	else if (c == 'd')
+	{}
+//		funtion
+	else if (c == 'i')	
+	{}
+//		ft_putnbr_fd(ft_atoi(arg), 1);
+	else if (c == 'u')
+		printf(" u ");
+	else if (c == 'x')
+		printf(" x ");
+	else if (c == 'X')
+		printf(" X ");
+	else if (c == '%')
+		printf(" XD ");
+}
+
+int ft_printf(char const *s, ...)
+{	
+	char *str;
+	char *word;
+	int	i;
+
+	va_list v_args;
+
+	va_start(v_args, s);
+	word = (char *)s;
+	i = 0;
+	while(s[i])
+	{
+		if(s[i] != '%')
+			ft_putchar_fd(s[i], 1);
+		else if(s[i] == '%' && ft_isalpha(s[i + 1]))
+		{
+			str = (char *)va_arg(v_args, char const *);
+			switch_function(word[i + 1], str);	
+			i++;
+		}
+		i++;
+	}
+	va_end(v_args);
+	return(0);
+}
 
 int	main()
 {
-	//ft_printf("este %s no sale", "hola");
-	putnbr_base(15, 16);
+	char *str = NULL;
+	
+	ft_printf("hola jajaj caracter: %c  string: %s  memory address: %p" "R" "hola" &str);
 	return (0);
 }
